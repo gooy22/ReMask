d50507089677465c37124df4ccababcf70ef38ce
gooy22 <sanekganggsta@gmail.com>
2026-09-21 11:24:08 +0300

    Bust creatives cache for Creative Meta capabilities

--- railway-v100-creatives.php
+++ railway-v100-creatives.php
@@ -376,7 +376,7 @@ require_once __DIR__ . '/checkpassword.php';
 </div>
 </div>
 
-<script src="scripts/creatives.js?v=20260919-meta-builder-v103" type="module"></script>
+<script src="scripts/creatives.js?v=20260921-creative-capabilities-v105" type="module"></script>
 <div class="app-footer"><?php include 'copyright.php' ?></div>
 </main>
 </body>
