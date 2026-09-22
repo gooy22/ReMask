3c2e8e695cffaffd7c7b844c0cf0068c7ef77bba
gooy22 <sanekganggsta@gmail.com>
2026-09-22 22:58:00 +0300

    Extend DTSG parser regression coverage

--- python_backend/tests/test_fb_worker_bootstrap.py
+++ python_backend/tests/test_fb_worker_bootstrap.py
@@ -25,6 +25,28 @@ class FacebookDtsgBootstrapParserTests(unittest.TestCase):
         source = r'[\"DTSGInitialData\",[],{\"token\":\"NA_js_token\"}]'
         self.assertEqual(self.extract(source), "NA_js_token")
 
+    def test_dtsg_init_data_payload(self) -> None:
+        source = (
+            '["DTSGInitData",[],'
+            '{"token":"NA_init_token","async_get_token":"NA_async"}]'
+        )
+        self.assertEqual(self.extract(source), "NA_init_token")
+
+    def test_dtsg_object_payload(self) -> None:
+        source = '{"dtsg":{"token":"NA_object_token"}}'
+        self.assertEqual(self.extract(source), "NA_object_token")
+
+    def test_named_json_input_payload(self) -> None:
+        source = '{"name":"fb_dtsg","value":"NA_named_token"}'
+        self.assertEqual(self.extract(source), "NA_named_token")
+
+    def test_unicode_escaped_init_payload(self) -> None:
+        source = (
+            r'[\\u0022DTSGInitData\\u0022,[],'
+            r'{\\u0022token\\u0022:\\u0022NA_unicode_token\\u0022}]'
+        )
+        self.assertEqual(self.extract(source), "NA_unicode_token")
+
     def test_hidden_input_payload(self) -> None:
         source = '<input type="hidden" name="fb_dtsg" value="NA_input_token">'
         self.assertEqual(self.extract(source), "NA_input_token")
