780c1a481e415085bfa0ec475c3fc8eb9517bfd8
gooy22 <sanekganggsta@gmail.com>
2026-09-24 00:20:28 +0300

    Test Meta SPA navigation abort recovery

--- python_backend/tests/test_business_browser_flow.py
+++ python_backend/tests/test_business_browser_flow.py
@@ -63,6 +63,78 @@ class BrowserNetworkGateTests(unittest.TestCase):
         )
 
 
+class BrowserNavigationRecoveryTests(unittest.IsolatedAsyncioTestCase):
+    async def test_err_aborted_is_accepted_when_meta_surface_is_alive(self):
+        class _Page:
+            def __init__(self):
+                self.url = "https://business.facebook.com/latest/home"
+                self.goto_calls = 0
+
+            async def goto(self, *args, **kwargs):
+                self.goto_calls += 1
+                raise Exception(
+                    "Page.goto: net::ERR_ABORTED; maybe frame was detached?"
+                )
+
+            async def wait_for_timeout(self, ms):
+                return None
+
+        browser = FacebookBusinessBrowser(
+            SimpleNamespace(profile_id="profile-nav")
+        )
+        browser.page = _Page()
+        browser._assert_authenticated = AsyncMock(return_value=None)
+        browser._body_text = AsyncMock(return_value="Meta Business Suite")
+        browser._form_ready = AsyncMock(return_value=False)
+        browser._has_create_surface = AsyncMock(return_value=False)
+
+        url = await browser._goto(
+            "https://business.facebook.com/reg/"
+        )
+
+        self.assertEqual(
+            url,
+            "https://business.facebook.com/latest/home",
+        )
+        self.assertEqual(browser.page.goto_calls, 1)
+
+    async def test_err_aborted_retries_once_when_replacement_dom_is_not_ready(self):
+        class _Page:
+            def __init__(self):
+                self.url = "https://business.facebook.com/reg/"
+                self.goto_calls = 0
+
+            async def goto(self, *args, **kwargs):
+                self.goto_calls += 1
+                if self.goto_calls == 1:
+                    raise Exception(
+                        "Page.goto: net::ERR_ABORTED; maybe frame was detached?"
+                    )
+                return None
+
+            async def wait_for_timeout(self, ms):
+                return None
+
+        browser = FacebookBusinessBrowser(
+            SimpleNamespace(profile_id="profile-nav-retry")
+        )
+        browser.page = _Page()
+        browser._assert_authenticated = AsyncMock(return_value=None)
+        browser._body_text = AsyncMock(side_effect=["", "Meta Business Suite"])
+        browser._form_ready = AsyncMock(return_value=False)
+        browser._has_create_surface = AsyncMock(return_value=False)
+
+        url = await browser._goto(
+            "https://business.facebook.com/reg/"
+        )
+
+        self.assertEqual(
+            url,
+            "https://business.facebook.com/reg/",
+        )
+        self.assertEqual(browser.page.goto_calls, 2)
+
+
 class BrowserPageDiscoveryTests(unittest.IsolatedAsyncioTestCase):
     async def test_discovers_pages_from_rendered_browser_surface(self):
         class _RenderedPage:
